8f9959f0b2b43056c1ad420ab82e663769fe9905
sandchick <yifan_liang@foxmail.com>
2022-05-12 22:38:57 +0800

    新增tmu总线和cordic算法

--- apb2tmu.v
+++ apb2tmu.v
@@ -8,16 +8,17 @@ module apb2tmu(
   output wire [31:0]  PRDATA,      // APB read data
   output wire PREADY,
   
-  input wire [11:0] data_cordic,
-  input wire [11:0] data_pid,
+  input wire [11:0] data_cordic_in,
+  input wire [11:0] data_pid_in,
+  output reg [11:0] data_cordic_out,
+  output reg [11:0] data_pid_in,
   output wire write_enablecordic,
+  output wire write_enablepid
 
 );
 reg [31:0] read_mux_byte;
 wire read_enable;
 wire write_enable;
-wire write_enablecordic;
-wire write_enablepid;
 
 // Read and write control signals
 assign  read_enable  = PSEL & (~PWRITE); // assert for whole APB read transfer
@@ -27,10 +28,27 @@ assign  write_enablepid = write_enable & (PADDR[11:2] == 10'h001);
 
 always @(*) begin
    case(PADDR[3:2])
-    2'b00: read_mux_word = data_cordic;
-    2'b01: read_mux_word = data_pid;
+    2'b00: read_mux_word = data_cordic_in;
+    2'b01: read_mux_word = data_pid_in;
     default: read_mux_word = {32{1'bx}};
    endcase
+end
+
+
+
+always @(posedge PCLK or negedge PRESETn) begin
+  if (~PRESETn)
+    data_cordic_out <= {12{1'b0}};
+  else if (write_enablecordic)
+    data_cordic_out <= PWDATA[11:0];
+end
+
+
+always @(posedge PCLK or negedge PRESETn) begin
+  if (~PRESETn)
+    data_pid_out <= {12{1'b0}};
+  else if (write_enablepid)
+    data_pid_out <= PWDATA[11:0];
 end
 
 assign PRDATA = (read_enable) ? read_mux_word : {32{1'b0}};
--- tmu.v
+++ tmu.v
@@ -1,24 +1,33 @@
 module tmu(
   input  wire        clk,    // PCLK for timer operation
   input  wire        rstn, // Reset
-  input wire [11:0] adc_data1,
-  input wire [11:0] adc_data2,
+  //input wire [11:0] adc_data1,
+  //input wire [11:0] adc_data2,
+  input wire [11:0] data_pid_in,
+  input wire [11:0] data_cordic_in,
   input wire write_enablecordic,
   input wire write_enablepid,
-  output wire [11:0] data_pid,
-  output wire [11:0] data_cordic,
+  output wire [11:0] data_pid_out,
+  output wire [11:0] data_cordic_out,
   output wire enable_pid,
   output wire enable_cordic
   ); 
 
-always @(posedge clk or negedge rstn) begin
-    if (~rstn)
-        
-end
+wire [11:0] data_pid;
+wire [11:0] data_cordic;
 
-always @(posedge clk or negedge rstn) begin
-    if (~rstn)
-    
-end
+assign data_pid = (write_enablepid) ? data_pid_in : {12{1'b0}};
+assign data_cordic = (write_enablecordic) ? data_cordic_in : {12{1'b0}};
+
+cordic u_cordic(
+     .clk (clk)
+    ,.atanin (data_cordic)
+    ,.atanout (data_cordic_out)
+);
+ 
+//pid u_pid(
+//
+//);
+
 
 endmodule
